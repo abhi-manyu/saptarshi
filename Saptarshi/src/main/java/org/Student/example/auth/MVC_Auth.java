106c6b6dc4a21ae34b6001460a5d7d7be05560ce
Abhi <abhim.moharana@gmail.com>
2019-09-30 14:06:38 +0530

    small changes for Saptarshi project.

---
+++ Saptarshi/src/main/java/org/Student/example/auth/MVC_Auth.java
@@ -0,0 +1,39 @@
+package org.Student.example.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class MVC_Auth extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	
+    	auth.inMemoryAuthentication()
+    	.withUser("user")
+    	.password("Reset@123")
+    	.roles("USER")
+    	.and()
+    	.withUser("admin")
+    	.password("abhi@manyu")
+    	.roles("ADMIN");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	
+    	http.httpBasic()
+    	.and()
+    	.authorizeRequests()
+    	.antMatchers(HttpMethod.GET,"/register/**").hasRole("ADMIN")
+    	.antMatchers(HttpMethod.POST,"/register/**").hasRole("ADMIN")
+    	.and()
+    	.csrf().disable()
+    	.formLogin().disable();
+    }
+}
